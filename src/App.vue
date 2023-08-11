42bb5f1dd89600cecd30b880d9274f2052269e83
unknown <yosof3553@gmail.com>
2023-08-11 22:29:47 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,100 @@
+<template>
+  <div class="registration-form">
+    <h2>Registration Form</h2>
+    <form @submit.prevent="submitForm">
+      <div class="form-group">
+        <label for="fullName">Full Name</label>
+        <input type="text" id="fullName" v-model="formData.fullName" required />
+      </div>
+      <div class="form-group">
+        <label for="email">Email Address</label>
+        <input type="email" id="email" v-model="formData.email" required />
+      </div>
+      <div class="form-group">
+        <label for="password">Password</label>
+        <input
+          type="password"
+          id="password"
+          v-model="formData.password"
+          required
+        />
+      </div>
+      <div class="form-group">
+        <label for="dob">Date of Birth</label>
+        <input type="date" id="dob" v-model="formData.dateOfBirth" required />
+      </div>
+      <div class="form-group">
+        <label for="gender">Gender</label>
+        <select id="gender" v-model="formData.gender" required>
+          <option value="">Select...</option>
+          <option value="male">Male</option>
+          <option value="female">Female</option>
+          <option value="other">Other</option>
+        </select>
+      </div>
+      <button type="submit">Register</button>
+    </form>
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+
+const formData = ref({
+  fullName: "",
+  email: "",
+  password: "",
+  dateOfBirth: "",
+  gender: "",
+});
+
+const submitForm = () => {
+  console.log("Form submitted:", formData.value);
+};
+</script>
+
+<style>
+.registration-form {
+  max-width: 400px;
+  margin: 50px auto;
+  padding: 20px;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.5);
+}
+
+.form-group {
+  margin-bottom: 15px;
+}
+
+label {
+  display: block;
+  font-weight: bold;
+  margin-bottom: 5px;
+}
+
+input[type="text"],
+input[type="email"],
+input[type="password"],
+input[type="date"],
+select {
+  width: 100%;
+  padding: 8px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+}
+
+input[type="date"],
+select {
+  width: 50%;
+}
+
+button {
+  background-color: #007bff;
+  color: #fff;
+  padding: 10px 15px;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+}
+</style>
